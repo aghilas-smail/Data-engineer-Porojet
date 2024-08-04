d6f881b32f7d8a5ad6dbfcfc19520f4945edfd97
Smail <aghilas.smail@gmail.com>
2024-08-04 18:12:51 +0200

    create a function to get the latest datatime from the data store in the json file

--- src/kafka/kafka_data.py
+++ src/kafka/kafka_data.py
@@ -1,1 +1,34 @@
 # Get and stream the data.
+# install kafka-python with this commande : pip install kafka-python
+
+import kafka.errors
+
+
+from src.data import(
+    URL_API,
+    Path,
+    DB_FIELDS,
+    Limit,
+    offsetlimit,
+)
+
+
+import json
+import datetime
+import requests # pip install requests
+from kafka import KafkaProducer
+from typing import List
+import logging
+
+
+# def to get the lastest timestamp.
+
+def get_latest_timestamp():
+   
+    
+    with open(Path, "r") as file: # Open the json file
+        data = json.load(file)
+        if "last_processed" in data:
+            return data["last_processed"]
+        else:
+            return datetime.datetime.min
