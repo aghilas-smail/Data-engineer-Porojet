79ed41d1068e65c447f25f77ca7909bce3aaed4b
Smail <aghilas.smail@gmail.com>
2024-08-04 17:24:26 +0200

    Postgre Param

---
+++ src/data.py
@@ -0,0 +1,16 @@
+import os
+
+# Param Api
+
+Path = "./data/last_processed.json"
+Limit = 100
+offsetlimit = 10000 - 1 - Limit
+
+
+# 3 Parametres to put in the URL of the API: Max_limit, date and offset/
+
+URL_API = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/rappelconso0/records?limit={}&where=date_de_publication%20%3E%20'{}'&order_by=date_de_publication%20DESC&offset={}"
+URL_API = URL_API.format(Limit, "{}","{}")
+
+
+# PostrSQL Paramet
