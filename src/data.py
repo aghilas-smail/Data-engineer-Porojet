cb8e30dc3f5e18954f2c3a84107490a5a8efe3df
Smail <aghilas.smail@gmail.com>
2024-08-04 17:31:08 +0200

    various aspects of PostgreSQL and database management

--- src/data.py
+++ src/data.py
@@ -22,3 +22,39 @@ Postgres_Properties = {
     "password": os.getenv("Postgres_Password"),
     "driver": "org.postgresql.Driver"
 }
+
+
+
+DB_FIELDS = [
+    "reference_fiche",
+    "ndeg_de_version",
+    "nature_juridique_du_rappel",
+    "categorie_de_produit",
+    "sous_categorie_de_produit",
+    "nom_de_la_marque_du_produit",
+    "noms_des_modeles_ou_references",
+    "identification_des_produits",
+    "conditionnements",
+    "date_debut_fin_de_commercialisation",
+    "temperature_de_conservation",
+    "marque_de_salubrite",
+    "informations_complementaires",
+    "zone_geographique_de_vente",
+    "distributeurs",
+    "motif_du_rappel",
+    "risques_encourus_par_le_consommateur",
+    "preconisations_sanitaires",
+    "description_complementaire_du_risque",
+    "conduites_a_tenir_par_le_consommateur",
+    "numero_de_contact",
+    "modalites_de_compensation",
+    "date_de_fin_de_la_procedure_de_rappel",
+    "informations_complementaires_publiques",
+    "liens_vers_les_images",
+    "lien_vers_la_liste_des_produits",
+    "lien_vers_la_liste_des_distributeurs",
+    "lien_vers_affichette_pdf",
+    "lien_vers_la_fiche_rappel",
+    "rappelguid",
+    "date_de_publication",
+]
