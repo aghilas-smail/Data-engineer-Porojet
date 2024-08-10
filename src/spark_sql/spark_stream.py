dd7e257da351490aa2db86268994ae0e352944c5
Smail <aghilas.smail@gmail.com>
2024-08-10 11:21:46 +0200

    inital spark_stream script, import all the packages

---
+++ src/spark_sql/spark_stream.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import(
+    StructType,
+    StructField, 
+    StringType,
+)
+from pyspark.sql.functions import from_json, col
+from src.data import Postgres_URL, Postgres_Properties, DB_FIELDS
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s:%(funcName)s:%(levelname)s:%(message)s"
+)
