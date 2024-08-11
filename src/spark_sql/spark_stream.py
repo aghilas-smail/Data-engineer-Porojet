b3e9bf38997fd3a2781bb02d6d37659149fe6189
Smail <aghilas.smail@gmail.com>
2024-08-11 18:26:36 +0200

    first and final data fram, need to chek if it's work

--- src/spark_sql/spark_stream.py
+++ src/spark_sql/spark_stream.py
@@ -12,4 +12,49 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s:%(funcName)s:%(levelname)s:%(message)s"
 )
 
-def create_spark_session()
+def create_spark_session() -> SparkSession:
+    spark = (
+        SparkSession.builder.appName("PostgreSQL CONNECTION WITH SPARK")
+        .config(
+            "spark.jars.packages",
+            "org.postgresql:postgresql:42.5.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
+            
+        )
+        .getOrCreate()
+    )
+    logging.info("Spark session created successfully")
+    return spark
+
+def create_initial_dataframe(spark_session):
+    """
+    create a initial dataframe from the streaming data (kafka)
+    """
+    try:
+        df = (
+            spark_session.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", "kafka:9092")
+            .option("subscribe", "rappel_conso")
+            .option("stratingoffsets", "earliest")
+            .load()
+        )
+        
+        logging.info("Inial dataframe created successfully")
+    except Exception as e:
+        logging.warning(f"Initial data frame coundn't be created due to expection : {e}")
+        raise
+    
+    return df
+
+def create_final_datafram(df):
+    schema = StructType(
+        [StructField(field_name, StringType(), True) for field_name in DB_FIELDS]
+    )
+    df_out = (
+        df.selectExpr("Test")
+        .select(from_json(col("value"), schema).alias("data"))
+        .select("data.*")
+    )
+    return df_out
+
+def start_streaming(df_parsed, spark):
+    
