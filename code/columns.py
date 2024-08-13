4e3b4e60e6bd1b512b8eae9bcfdf281b3f8282bd
Smail <aghilas.smail@gmail.com>
2024-08-13 21:20:26 +0200

    add new column to the data base automatically

---
+++ code/columns.py
@@ -0,0 +1,46 @@
+import os 
+import psycopg2
+
+from src.data import DB_FIELDS
+
+# Data base parameters
+
+dbname = "postgres"
+user = "postgres"
+password = os.getenv("POSTRES_PASSWORD")
+host = "localhost"
+
+# connection to the data base
+
+conn  = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+cur = conn.cursor()
+
+
+def try_execut_sql(sql: str):
+    try:
+        cur.execute(sql)
+        conn.commit()
+        print(f"Executed {sql} successfully")
+    except Exception as e:
+        print(f"Couldn't execute {sql} due to exception: {e}")
+        conn.rollback()
+        
+def alter_table():
+    primary_key_sql = f"""
+    ALTER TABLE rappel_conso
+    ADD COLUMN KEY (DB_FIELDS[0]);
+    """
+    try_execut_sql(primary_key_sql)
+    for field in DB_FIELDS[1:]:
+        alter_table_sql = f"""
+        ALTER TABLE rapport_conso
+        ADD COLUMN {field} text;
+        """
+        try_execut_sql(alter_table_sql)
+        
+    cur.close()
+    conn.close()
+    
+    
+if __name__ == "__main__":
+    alter_table()
